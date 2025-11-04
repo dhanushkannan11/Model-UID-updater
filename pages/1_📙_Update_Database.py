2d9259e734171b0fccd82ca9d45a3609cebfd43c
dhanushkannan11 <dhanushkannan001@gmail.com>
2025-11-04 16:20:47 +0530

    Generate UID List

--- pages/1_📙_Update_Database.py
+++ pages/1_📙_Update_Database.py
@@ -4,6 +4,8 @@ from packages.main import *
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 import boto3
 import time 
+from dotenv import load_dotenv
+load_dotenv()
 
 service_name = "dynamodb"
 region = os.getenv('AWS_REGION')
@@ -15,6 +17,7 @@ access_key = os.getenv('AWS_ACCESS_KEY')
 st.sidebar.page_link('Generate_UID.py', label='🆔 Generate UID')
 st.sidebar.page_link('pages/1_📙_Update_Database.py', label='📙 Update Database')
 st.sidebar.page_link('pages/2_📈_Visualize_Data.py', label='📈 Visualize Data')
+st.sidebar.page_link('pages/3_📃_Generate_UID_List.py', label='📃 Generate UID List')
 
 def main():
     """
